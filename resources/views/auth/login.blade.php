0a661430ba6868b2b93fa9f5fdd6f5c4a9ed9350
Juanbpp <juanbpp1@gmail.com>
2024-05-28 21:38:45 +0200

    login errores

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,8 +23,9 @@
 <div class="card-body login-card-body">
 <p class="login-box-msg">Logago</p>
 <form action="{{ route('login') }}" method="post">
+ @csrf       
 <div class="input-group mb-3">
-<input type="email" class="form-control" placeholder="Email">
+<input id="email" name="email" type="email" class="form-control" placeholder="Email">
 <div class="input-group-append">
 <div class="input-group-text">
 <span class="fas fa-envelope"></span>
@@ -32,7 +33,7 @@
 </div>
 </div>
 <div class="input-group mb-3">
-<input type="password" class="form-control" placeholder="Password">
+<input type="password"  id="password"  name="password" class="form-control" placeholder="Password">
 <div class="input-group-append">
 <div class="input-group-text">
 <span class="fas fa-lock"></span>
