b0f0076e48c8add30fd289d4a7e5cb7def8069c5
Juanbpp <juanbpp1@gmail.com>
2024-06-01 22:45:22 +0200

    diseño3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,56 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="content">
+    <div class="container-fluid">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="row">
+            <div class="col-lg-3 col-6">
 
-                    {{ __('You are logged in!') }}
+                <div class="small-box bg-info">
+                    <center><a href="/yoigo"><img src="./yoigo2.png"></a>
                 </div>
             </div>
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-info">
+                    <center><a href="/masmovil"><img src="./masmovil2.png"></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-info">
+                    <center><a href="/movistar"><img src="./movistar2.png"></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-info">
+                    <center><a href="/vodafone"><img src="./vodafone2.png"></a>
+                </div>
+            </div>
+
+
+        </div>
+
+
+        <div class="row">
+
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-info">
+                    <center><a href="/orange"><img src="./orange2.png"></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+
+                <div class="small-box bg-info">
+                    <center><a href="/jazztel"><img src="./jazztel2.png"></a>
+                </div>
+            </div>
+
         </div>
     </div>
-</div>
+</div>    
 @endsection
