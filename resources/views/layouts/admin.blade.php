992caea4ac14e7fb03470066d9a2a077f08b6bb4
Juanbpp <juanbpp1@gmail.com>
2024-06-02 10:32:57 +0200

    login5

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -340,7 +340,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <footer class="main-footer">
             <!-- To the right -->
             <div class="float-right d-none d-sm-inline">
-                Anything you want
             </div>
             <!-- Default to the left -->
             <strong>Copyright &copy; 2024 <a href="https://kalculadora-production.up.railway.app">Kalculadora</a>.</strong> All rights
