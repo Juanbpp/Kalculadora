ea20007ceb28d9182d477cd6ab2f121b29110385
Juanbpp <juanbpp1@gmail.com>
2024-06-02 10:39:22 +0200

    login6

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,6 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kalculator</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('/dist/img/AdminLTELogo.png') }}">
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
