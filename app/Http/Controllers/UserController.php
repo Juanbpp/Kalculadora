75759926c94b050a55b4cc6765945f0e89d05a32
Juanbpp <juanbpp1@gmail.com>
2024-06-04 22:26:45 +0200

    detalle

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
         'password' => Hash::make($request['password']),
         ]);
 
-        return redirect()->route('user.index')
+        return redirect()->route('users.index')
             ->with('success', 'Tipo created successfully.');
     }
 
