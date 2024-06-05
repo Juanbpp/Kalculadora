337a179076adf9bcc30cb5789ebde38fbd084163
Juanbpp <juanbpp1@gmail.com>
2024-06-05 18:20:07 +0200

    detalle

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -14,7 +14,7 @@
                         <span class="card-title">{{ __('Update') }} User</span>
                     </div>
                     <div class="card-body bg-white">
-                        <form method="POST" action="{{ route('users.update', {{ Auth::user()->id }}) }}"  role="form" enctype="multipart/form-data">
+                        <form method="POST" action="{{ route('users.update', Auth::user()->id ) }}"  role="form" enctype="multipart/form-data">
                             {{ method_field('PATCH') }}
                             @csrf
 
