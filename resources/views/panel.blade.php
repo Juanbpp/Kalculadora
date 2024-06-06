81a289c650ef46fd09723b38c93b6ef892729439
Juanbpp <juanbpp1@gmail.com>
2024-06-06 17:36:23 +0200

    detalle2

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -61,15 +61,9 @@
                             <div style="display: flex; justify-content: space-between; align-items: center;">
     
                                 <span id="card_title">
-                                    {{ __('User') }}
+                                    {{ __('Registro') }}
                                 </span>
     
-                                <div class="float-right">
-                                    <a href="{{ route('users.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                      {{ __('Crear nuevo') }}
-                                    </a>
-                                  </div>
-         
                             </div>
                         </div>
                         @if ($message = Session::get('success'))
@@ -85,27 +79,97 @@
                                         <tr>
                                             <th>No</th>
                                             
-                                            <th>Name</th>
-                                            <th>Email</th>
+                                            <th>Dni</th>
+                                            <th>Telefono</th>
+                                            <th>Impuesto</th>
+                                            <th>Id Net</th>
+                                            <th>Id Convergente</th>
+                                            <th>Id Tv</th>
+                                            <th>Id Descuento</th>
+                                            <th>Id Adicional 1</th>
+                                            <th>Id Adicional 2</th>
+                                            <th>Id Adicional 3</th>
+                                            <th>Id Adicional 4</th>
+                                            <th>Id Adicional 5</th>
+                                            <th>Cantidad Adicional 1</th>
+                                            <th>Cantidad Adicional 2</th>
+                                            <th>Cantidad Adicional 3</th>
+                                            <th>Cantidad Adicional 4</th>
+                                            <th>Cantidad Adicional 5</th>
+                                            <th>Id Movil 1</th>
+                                            <th>Id Movil 2</th>
+                                            <th>Id Movil 3</th>
+                                            <th>Id Movil 4</th>
+                                            <th>Id Movil 5</th>
+                                            <th>Cantidad Movil 1</th>
+                                            <th>Cantidad Movil 2</th>
+                                            <th>Cantidad Movil 3</th>
+                                            <th>Cantidad Movil 4</th>
+                                            <th>Cantidad Movil 5</th>
+                                            <th>Descuento Movil 1</th>
+                                            <th>Descuento Movil 2</th>
+                                            <th>Descuento Movil 3</th>
+                                            <th>Descuento Movil 4</th>
+                                            <th>Descuento Movil 5</th>
+                                            <th>Pvp</th>
+                                            <th>Codificacion</th>
+                                            <th>Observaciones</th>
+                                            <th>Id Codificacion</th>
+                                            <th>Id User</th>
+                                            <th>Creado</th>
     
                                             <th></th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach ($users as $user)
+                                        @foreach ($registros as $registro)
                                             <tr>
-                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $loop->iteration}}</td>
                                                 
-                                                <td>{{ $user->name }}</td>
-                                                <td>{{ $user->email }}</td>
+                                                <td>{{ $registro->DNI }}</td>
+                                                <td>{{ $registro->telefono }}</td>
+                                                <td>{{ $registro->impuesto }}</td>
+                                                <td>{{ $registro->id_net }}</td>
+                                                <td>{{ $registro->id_convergente }}</td>
+                                                <td>{{ $registro->id_tv }}</td>
+                                                <td>{{ $registro->id_descuento }}</td>
+                                                <td>{{ $registro->id_adicional_1 }}</td>
+                                                <td>{{ $registro->id_adicional_2 }}</td>
+                                                <td>{{ $registro->id_adicional_3 }}</td>
+                                                <td>{{ $registro->id_adicional_4 }}</td>
+                                                <td>{{ $registro->id_adicional_5 }}</td>
+                                                <td>{{ $registro->cantidad_adicional_1 }}</td>
+                                                <td>{{ $registro->cantidad_adicional_2 }}</td>
+                                                <td>{{ $registro->cantidad_adicional_3 }}</td>
+                                                <td>{{ $registro->cantidad_adicional_4 }}</td>
+                                                <td>{{ $registro->cantidad_adicional_5 }}</td>
+                                                <td>{{ $registro->id_movil_1 }}</td>
+                                                <td>{{ $registro->id_movil_2 }}</td>
+                                                <td>{{ $registro->id_movil_3 }}</td>
+                                                <td>{{ $registro->id_movil_4 }}</td>
+                                                <td>{{ $registro->id_movil_5 }}</td>
+                                                <td>{{ $registro->cantidad_movil_1 }}</td>
+                                                <td>{{ $registro->cantidad_movil_2 }}</td>
+                                                <td>{{ $registro->cantidad_movil_3 }}</td>
+                                                <td>{{ $registro->cantidad_movil_4 }}</td>
+                                                <td>{{ $registro->cantidad_movil_5 }}</td>
+                                                <td>{{ $registro->descuento_movil_1 }}</td>
+                                                <td>{{ $registro->descuento_movil_2 }}</td>
+                                                <td>{{ $registro->descuento_movil_3 }}</td>
+                                                <td>{{ $registro->descuento_movil_4 }}</td>
+                                                <td>{{ $registro->descuento_movil_5 }}</td>
+                                                <td>{{ $registro->pvp }}</td>
+                                                <td>{{ $registro->codificacion }}</td>
+                                                <td>{{ $registro->observaciones }}</td>
+                                                <td>{{ $registro->id_codificacion }}</td>
+                                                <td>{{ $registro->id_user }}</td>
+                                                <td>{{ $registro->created_at }}</td>
     
                                                 <td>
-                                                    <form action="{{ route('users.destroy',$user->id) }}" method="POST">
-                                                        <a class="btn btn-sm btn-primary " href="{{ route('users.show',$user->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Mostrar') }}</a>
-                                                        <a class="btn btn-sm btn-success" href="{{ route('users.edit',$user->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}</a>
+                                                    <form action="{{ route('registros.destroy',$registro->id) }}" method="POST">
+                                                        <a class="btn btn-sm btn-primary " href="{{ route('registros.show',$registro->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Mostrar') }}</a>
+                                                        <a class="btn btn-sm btn-success" href="{{ route('registros.edit',$registro->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}</a>
                                                         @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Borrar') }}</button>
                                                     </form>
                                                 </td>
                                             </tr>
@@ -113,9 +177,9 @@
                                     </tbody>
                                 </table>
                             </div>
-                        </div>
                     </div>
                 </div>
+            </div>
         </div>
         <script>
             $(function () {
