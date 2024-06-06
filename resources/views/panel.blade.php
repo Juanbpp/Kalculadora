98032f5a485210898bb17ea0d387306b72ba562b
Juanbpp <juanbpp1@gmail.com>
2024-06-06 16:02:12 +0200

    detalle2

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -54,7 +54,121 @@
                 </div>
             </div>
 
-
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div style="display: flex; justify-content: space-between; align-items: center;">
+    
+                                <span id="card_title">
+                                    {{ __('User') }}
+                                </span>
+    
+                                <div class="float-right">
+                                    <a href="{{ route('users.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                                      {{ __('Crear nuevo') }}
+                                    </a>
+                                  </div>
+         
+                            </div>
+                        </div>
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success m-4">
+                                <p>{{ $message }}</p>
+                            </div>
+                        @endif
+    
+                        <div class="card-body bg-white">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead class="thead">
+                                        <tr>
+                                            <th>No</th>
+                                            
+                                            <th>Name</th>
+                                            <th>Email</th>
+    
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($users as $user)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                
+                                                <td>{{ $user->name }}</td>
+                                                <td>{{ $user->email }}</td>
+    
+                                                <td>
+                                                    <form action="{{ route('users.destroy',$user->id) }}" method="POST">
+                                                        <a class="btn btn-sm btn-primary " href="{{ route('users.show',$user->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Mostrar') }}</a>
+                                                        <a class="btn btn-sm btn-success" href="{{ route('users.edit',$user->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}</a>
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Borrar') }}</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <script>
+            $(function () {
+                                    $("#example1").DataTable({
+                                        "pageLength": 10,
+                                        "language": {
+                                            "emptyTable": "No hay información",
+                                            "info": "Mostrando _START_ a _END_ de _TOTAL_ ",
+                                            "infoEmpty": "Mostrando 0 a 0 de 0 ",
+                                            "infoFiltered": "(Filtrado de _MAX_ total)",
+                                            "infoPostFix": "",
+                                            "thousands": ",",
+                                            "lengthMenu": "Mostrar _MENU_ ",
+                                            "loadingRecords": "Cargando...",
+                                            "processing": "Procesando...",
+                                            "search": "Buscador:",
+                                            "zeroRecords": "Sin resultados encontrados",
+                                            "paginate": {
+                                                "first": "Primero",
+                                                "last": "Ultimo",
+                                                "next": "Siguiente",
+                                                "previous": "Anterior"
+                                            }
+                                        },
+                                        "responsive": true, "lengthChange": true, "autoWidth": false,
+                                        buttons: [{
+                                            extend: 'collection',
+                                            text: 'Reportes',
+                                            orientation: 'landscape',
+                                            buttons: [{
+                                                text: 'Copiar',
+                                                extend: 'copy',
+                                            }, {
+                                                extend: 'pdf'
+                                            },{
+                                                extend: 'csv'
+                                            },{
+                                                extend: 'excel'
+                                            },{
+                                                text: 'Imprimir',
+                                                extend: 'print'
+                                            }
+                                            ]
+                                        },
+                                            {
+                                                extend: 'colvis',
+                                                text: 'Visor de columnas',
+                                            }
+                                        ],
+                                    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+                                });      </script>
+    
+    
 
             
         </div>
